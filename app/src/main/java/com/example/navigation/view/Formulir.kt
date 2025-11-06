060211b56f0a5634355ced6a3b653985c0e81d2f
ZhilPro27 <zhilalkrisna@gmail.com>
2025-11-06 13:08:30 +0700

    Membuat fungsi FormIsian di dalam file Formulir.kt

---
+++ app/src/main/java/com/example/navigation/view/Formulir.kt
@@ -0,0 +1,14 @@
+@file:OptIn(ExperimentalMaterial3Api::class)
+
+package com.example.navigation.view
+
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.runtime.Composable
+
+@Composable
+fun FormIsian(
+
+) {
+
+}
+
