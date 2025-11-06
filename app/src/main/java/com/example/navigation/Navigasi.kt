f88aacc96efe74593a8e454c38d7baf89d1a178e
ZhilPro27 <zhilalkrisna@gmail.com>
2025-11-06 12:45:25 +0700

    Menambahkan fungsi private bernama cancelAndBackToFormulirku di dalam Navigasi.kt

--- app/src/main/java/com/example/navigation/Navigasi.kt
+++ app/src/main/java/com/example/navigation/Navigasi.kt
@@ -42,3 +42,9 @@ fun DataApp(
         }
     }
 }
+
+private fun cancelAndBackToFormulirku(
+    navController: NavHostController
+) {
+    navController.popBackStack(Navigasi.Formulirku.name, inclusive = false)
+}
